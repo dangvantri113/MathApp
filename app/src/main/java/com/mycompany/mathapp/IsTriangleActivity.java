3e88b8fa4d812b91b363a0c8ceb6974c4d7f249c
dangvantri113 <31469796+dangvantri113@users.noreply.github.com>
2017-09-17 11:45:18 +0700

    version2
    tri

---
+++ app/src/main/java/com/mycompany/mathapp/IsTriangleActivity.java
@@ -0,0 +1,13 @@
+package com.mycompany.mathapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IsTriangleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is_triangle);
+    }
+}
