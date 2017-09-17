b1a2d57fc34fd53cb6d99e6ac038fa97524c6304
dangvantri113 <31469796+dangvantri113@users.noreply.github.com>
2017-09-17 21:45:58 +0700

    version2

--- README.md
+++
@@ -1,7 +0,0 @@
-# MathApp
-study
-team 8
-đặng văn trí
-lê văn minh
-hồ anh tuấn
-cái thế sỹ
--- app/src/main/java/com/mycompany/mathapp/ShapeActivity.java
+++ app/src/main/java/com/mycompany/mathapp/ShapeActivity.java
@@ -1,20 +1,13 @@
 package com.mycompany.mathapp;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class ShapeActivity extends AppCompatActivity {
-
-//    private ListView lv;
-//    private String[] number = {"HÌNH TAM GIÁC","HÌNH VUÔNG","HÌNH TRÒN","ĐOẠN THẲNG","ĐO ĐỌ DÀI ĐOẠN THẲNG","ĐIỂM VÀ ĐOẠN THẲNG","ĐẾM HÌNH TAM GIÁC","ĐẾM HÌNH VUÔNG","ĐIỂM TRONG VÀ NGOÀI HÌNH TRÒN"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        lv= (ListView) findViewById(R.id.lvPhoneNumber);
-//        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,number);
-//
-//        lv.setAdapter(arrayAdapter);
+        setContentView(R.layout.activity_shape);
     }
 }
